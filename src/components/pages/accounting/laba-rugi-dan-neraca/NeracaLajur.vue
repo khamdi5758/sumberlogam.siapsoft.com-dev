960815b83b43847faa0b39e68828cfb8d316059a
Ahmad Nur Fuady <ahmadnurfuady23@gmail.com>
2026-07-29 11:32:38 +0700

    refactor(accounting): update NeracaLajur.vue to fetch data from Vuex store

--- src/components/pages/accounting/laba-rugi-dan-neraca/NeracaLajur.vue
+++ src/components/pages/accounting/laba-rugi-dan-neraca/NeracaLajur.vue
@@ -13,22 +13,21 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed } from "vue";
+import { useStore } from "vuex";
 import AccountingBase from "../AccountingBase.vue";
 
 defineOptions({
   name: "RegisterNeracaLajur",
 });
 
-const plData = ref([
-  { id: 1, parentId: 0, accountName: "KAS & BANK", amount: 155000000, amountBulanLalu: 140000000, type: "header" },
-  { id: 11, parentId: 1, accountName: "Kas Kecil", amount: 120000000, amountBulanLalu: 110000000, type: "detail" },
-  { id: 12, parentId: 1, accountName: "Bank BCA", amount: 35000000, amountBulanLalu: 30000000, type: "detail" },
-  { id: 19, parentId: 1, accountName: "Total Kas & Bank", amount: 155000000, amountBulanLalu: 140000000, type: "subtotal" }
-]);
+const store = useStore();
+
+// Map data source reactively to Vuex store state
+const plData = computed(() => store.getters["neracalajur/neracalajurList"] || []);
 
 const handleFilterChange = (filterData) => {
   console.log("Neraca Lajur Filter Change:", filterData);
-  // Logika fetch data Neraca Lajur
+  store.dispatch("neracalajur/getNeracalajur", filterData);
 };
 </script>
