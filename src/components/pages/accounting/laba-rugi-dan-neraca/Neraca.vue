e250d9020bb9830370b679619de007711e40edcb
Ahmad Nur Fuady <ahmadnurfuady23@gmail.com>
2026-07-29 11:31:06 +0700

    refactor(accounting): update Neraca.vue to fetch data from Vuex store

--- src/components/pages/accounting/laba-rugi-dan-neraca/Neraca.vue
+++ src/components/pages/accounting/laba-rugi-dan-neraca/Neraca.vue
@@ -13,28 +13,21 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed } from "vue";
+import { useStore } from "vuex";
 import AccountingBase from "../AccountingBase.vue";
 
 defineOptions({
   name: "RegisterNeraca",
 });
 
-const plData = ref([
-  { id: 1, parentId: 0, accountName: "AKTIVA (ASET)", amount: 350000000, amountBulanLalu: 310000000, type: "header" },
-  { id: 11, parentId: 1, accountName: "Kas & Bank", amount: 155000000, amountBulanLalu: 140000000, type: "detail" },
-  { id: 12, parentId: 1, accountName: "Piutang Usaha", amount: 120000000, amountBulanLalu: 110000000, type: "detail" },
-  { id: 13, parentId: 1, accountName: "Persediaan Barang", amount: 75000000, amountBulanLalu: 60000000, type: "detail" },
-  { id: 19, parentId: 1, accountName: "Total Aktiva", amount: 350000000, amountBulanLalu: 310000000, type: "subtotal" },
-  { id: 2, parentId: 0, accountName: "PASIVA (KEWAJIBAN & MODAL)", amount: 350000000, amountBulanLalu: 310000000, type: "header" },
-  { id: 21, parentId: 2, accountName: "Hutang Usaha", amount: 90000000, amountBulanLalu: 80000000, type: "detail" },
-  { id: 22, parentId: 2, accountName: "Modal Saham", amount: 200000000, amountBulanLalu: 200000000, type: "detail" },
-  { id: 23, parentId: 2, accountName: "Laba Ditahan", amount: 60000000, amountBulanLalu: 30000000, type: "detail" },
-  { id: 29, parentId: 2, accountName: "Total Pasiva", amount: 350000000, amountBulanLalu: 310000000, type: "subtotal" }
-]);
+const store = useStore();
+
+// Map data source reactively to Vuex store state
+const plData = computed(() => store.getters["neraca/neracaList"] || []);
 
 const handleFilterChange = (filterData) => {
   console.log("Neraca Filter Change:", filterData);
-  // Logika fetch data Neraca
+  store.dispatch("neraca/getNeraca", filterData);
 };
 </script>
