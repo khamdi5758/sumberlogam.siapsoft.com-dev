7a47f775b26e18e8e1f6e4c588cfdcce340d86c2
Ahmad Nur Fuady <ahmadnurfuady23@gmail.com>
2026-08-23 21:24:05 +0700

    feat: implement KartuPiutang page component

---
+++ src/components/pages/Finance/Kartu/KartuPiutang.vue
@@ -0,0 +1,15 @@
+<template>
+  <KartuBase
+    title="Kartu Piutang"
+    type="piutang"
+    storeModule="kartupiutang"
+    initialPerkiraan="130.01"
+  />
+</template>
+
+<script setup>
+import KartuBase from "./KartuBase.vue";
+defineOptions({
+  name: "KartuPiutang",
+});
+</script>
